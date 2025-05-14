4a714cc3d0447d466bedeffde6f5883264cc49ce
Khrist Mae Camoc <khristmaeamoc@gmail.com>
2025-05-14 15:01:56 +0800

    Initial backend code

---
+++ app/database.py
@@ -0,0 +1,35 @@
+# app/database.py
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+import logging
+
+# Configure logging
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set")
+
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=True,  # This will show SQL queries in console
+    pool_pre_ping=True  # Checks connection before using
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
